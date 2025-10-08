8d8b81c5cbae1d0da0eb5ee70c1a62d0b2c2579c
RahulR015 <rahulr15ec@gmail.com>
2025-10-08 10:20:55 +0530

    Created User Entity

--- src/main/java/com/example/SmartEventManagementSystem/Entities/User.java
+++ src/main/java/com/example/SmartEventManagementSystem/Entities/User.java
@@ -1,3 +1,40 @@
 package com.example.SmartEventManagementSystem.Entities;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+
 public class User {
+    public enum Role {USER, ADMIN}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable=false)
+    @NotBlank(message = "Email Cannot be empty")
+    @Email(message = "Invalid Email formate")
+    private String email;
+    @Column(nullable=false)
+    @NotBlank(message = "Password Cannot be empty")
+    private String password;
+    @Column(nullable=false)
+    @NotBlank(message = "Name Cannot be empty")
+    private String name;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable=false)
+    private Role role=Role.USER;
+    @ManyToMany(mappedBy = "user",cascade=CascadeType.ALL)
+    private List<Registration> registrations;
+    @ManyToMany(mappedBy = "event",cascade=CascadeType.ALL)
+    private List<Event> events;
+
+
+
+
 }
